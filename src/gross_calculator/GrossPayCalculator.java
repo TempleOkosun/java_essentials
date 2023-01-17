2caa824861252ea4b28ac66dbb0d9a89b7853912
Temple <okosun.temple@gmail.com>
2023-01-17 00:56:02 -0500

    updating java skills

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,4 +1,21 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
+
+    public static  void main(String[] args){
+        // get the hours worked
+        int hours = 0;
+        System.out.println("How many hours did you work?:");
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
+        double payRate = 0;
+        System.out.println("What is your pay rate?:");
+        payRate = scanner.nextDouble();
+        double grossPay = hours * payRate;
+
+        System.out.println("Gross Pay: " + grossPay);
+
+    }
 }
